c002f606a78f947eec3c002dce9910eb86a59830
Harry Payne <102956247+HarryPayne2@users.noreply.github.com>
2022-04-26 07:53:56 +0100

    Update exercise3.py

--- harry-python/exercise3.py
+++ harry-python/exercise3.py
@@ -3,9 +3,10 @@ Unit = input("(K)g or (L)bs: ")
 if Unit.upper() == "K":
     converted = Weight / 0.45
     print("Weight in Lbs: " + str(converted))
-else:
+elif Unit.upper() == "L":
     converted = Weight * 0.45
     print("Weight in Kgs: " + str(converted))
+else:print("Please enter a valid unit")
 
 
 
