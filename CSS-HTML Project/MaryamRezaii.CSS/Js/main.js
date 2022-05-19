4001e923bbeffa060714dbc3a823e6733b27387a
MaryamRezaii <102955229+MaryamRezaii@users.noreply.github.com>
2022-05-19 18:04:22 +0100

    Maryam CSS

---
+++ CSS-HTML Project/MaryamRezaii.CSS/Js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    $nav = $('.nav');
+    $toggleCollapse = $('.toggle-collapse');
+
+    /** click event on toggle menu */
+    $toggleCollapse.click(function () {
+        $nav.toggleClass('collapse');
+    })
+
+// owl-crousel for blog
+$('.owl-carousel').owlCarousel({
+
+});
