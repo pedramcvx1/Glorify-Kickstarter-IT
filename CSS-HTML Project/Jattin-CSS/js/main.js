a97b9329e4e33818ce54393c0588b007a0a47704
JattinP <102956886+JattinP@users.noreply.github.com>
2022-05-24 09:47:57 +0100

    Update3

--- CSS-HTML Project/Jattin-CSS/js/main.js
+++ CSS-HTML Project/Jattin-CSS/js/main.js
@@ -1,3 +1,20 @@
+const responsive = {
+  0: {
+    items: 1
+  },
+  320: {
+    items: 1
+  },
+  560: {
+    items: 2
+  },
+  960: {
+    items: 3
+  }
+}
+
+
+
 $(document).ready(function(){
 
  $nav = $('.nav');
@@ -15,7 +32,8 @@ $('.owl-carousel').owlCarousel({
     autoplayTimeout: 3000,
     dots: false,
     nav: true,
-    navText:[$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
+    navText:[$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+    responsive: responsive
   }); 
 
 
