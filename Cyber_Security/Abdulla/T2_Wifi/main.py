fe530f723d255f09ba77dc33d289f6514dc7fdc9
Abdulla Sulieman <102958241+AbdullaAC@users.noreply.github.com>
2022-07-01 10:30:43 +0100

    T2_Wifi

---
+++ Cyber_Security/Abdulla/T2_Wifi/main.py
@@ -0,0 +1,42 @@
+from cProfile import Profile
+import subprocess
+
+import re
+
+command_output = subprocess.run(["netsh", "wlan", "show", "profiles"], capture_output = True).stdout.decode()
+
+Profile_names = (re.fndall("all user profile  : (,*)\r", command_output))
+
+wifi_list = list()
+
+
+if len(Profile_names) != 0:
+    for name in Profile_names:
+
+        wifi_profile = dict()
+
+        profile_info = subprocess.run(["netsh", "wlan", "show", "profile", name],  capture_output = True).stdout.decode
+
+        if re.search("Security key   : Absent", profile_info):
+            continue
+        else:
+
+            wifi_profile["ssid"] =  name
+            
+            profile_info_pass = subprocess.run(["netsh", "wlan", "show", "profile", name, "key=clear"],  capture_output = True).stdout.decode()
+
+            password = re.search("key content   : (.*)\r", profile_info_pass)
+
+            if password == None:
+                wifi_profile["password"] = None
+            else:
+
+                wifi_profile["password"] = password[1]
+
+                wifi_list.append(wifi_profile)
+
+
+for x in range(len(wifi_list)):
+    print(wifi_list[x])
+
+
